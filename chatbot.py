62cf88444d28565280b41d4561271f9a69bf42e1
floatedbloom <georgelapin23@gmail.com>
2025-05-10 12:57:12 -0700

    refactor

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,10 @@
+import streamlit as st
+import numpy as np
+import asyncio
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
 import os
 import speech_recognition as sr
-import streamlit as st
 
 load_dotenv()
 client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -46,3 +48,15 @@ def get_audio_input():
         except sr.RequestError as e:
             st.write(f"Could not request results; {e}")
     return ""
+
+def chatbot():
+    chatbox = st.container(height=800)
+    if st.button("Speak your query"):
+        prompt = get_audio_input()
+        if prompt:
+            with chatbox.chat_message("user"):
+                st.markdown(prompt)
+            with st.spinner("Analyzing your input..."):
+                response = asyncio.run(generate_chat_analysis(prompt))
+            with chatbox.chat_message("assistant"):
+                st.markdown(response)
--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 import streamlit as st
 from body_diagram import render_body_diagram
+from chatbot import chatbot
 import numpy as np
 import asyncio
-from tools import generate_chat_analysis, get_audio_input
-
 
 st.title("PhizzyAI")
 
-st.write("Placeholder for the body diagram")
-
-def chatbot():
-    chatbox = st.container(height=800)
-    if st.button("Speak your query"):
-        prompt = get_audio_input()
-        if prompt:
-            with chatbox.chat_message("user"):
-                st.markdown(prompt)
-            with st.spinner("Analyzing your input..."):
-                response = asyncio.run(generate_chat_analysis(prompt))
-            with chatbox.chat_message("assistant"):
-                st.markdown(response)
+render_body_diagram()
 
 chatbot()
